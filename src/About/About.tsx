d84799c7c856f95d8bf3fa5e02e2f141a39e59d3
Jessica Roy <2669123+bouncingleaf@users.noreply.github.com>
2023-08-12 14:20:34 -0400

    Update content

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,6 +1,36 @@
+import { Link } from "react-router-dom";
+
 const About = () => (
   <div>
-    Yo, this is all you need to know about Leaf.
+    <h1>Hi! I'm Leaf.</h1>
+    <div className='card'>
+      <p>
+        I've been working in technology for 25+ years, and I've been a developer for most of that time. My current title is Application Architect. While I do help dev teams with their application architecture, my passion is making life and work easier and more joyful for my developer colleagues.
+      </p>
+      <p>
+        My current focus is on application and process observability. I'm a huge fan of the systems and organizational aspects of DevOps -- to me, DevOps is the set of collaborative practices for helping deliver value faster and safer. That you get happier dev teams in the process only makes it better.
+      </p>
+      <p>When I'm not staring at a screen, I like to look at birds, walk outside, and listen to audio books or music, sometimes all at once. I also spend a lot of time attaching lines to paper.
+      </p>
+    </div>
+
+    <h1>Learning stuff</h1>
+    <div className="card">
+      <p>
+        Here are some of the books I've been reading lately:
+      </p>
+      <ul>
+        <li><Link to="https://itrevolution.com/product/the-phoenix-project/">The Phoenix Project</Link> - start here.</li>
+        <li><Link to="https://itrevolution.com/product/the-devops-handbook-second-edition/">The DevOps Handbook</Link> - the practical companion to The Phoenix Project.</li>
+        <li><Link to="https://itrevolution.com/product/team-topologies/">Team Topologies</Link></li>
+        <li><Link to="https://itrevolution.com/product/project-to-product/">Project to Product</Link></li>
+        <li><Link to="https://itrevolution.com/product/accelerate/">Accelerate</Link></li>
+        <li><Link to="https://info.honeycomb.io/observability-engineering-oreilly-book-2022">Observability Engineering</Link> - my handbook right now for working with observability</li>
+      </ul>
+      <p>
+        I also highly recommend the videos at <Link to="https://videos.itrevolution.com/">ITRevolution.com</Link> - signup is free and it gets you 10 free videos per month.
+      </p>
+    </div>
   </div>
 );
 
--- src/Blog/Blog.tsx
+++ src/Blog/Blog.tsx
@@ -1,5 +1,35 @@
 const Blog = () => (
   <div>
+    <h2>Thinking errors</h2>
+    <p>
+      Notes from a <a href="https://www.linkedin.com/posts/sheril-mathews_common-thinking-errors-activity-7089957573222694912-tXb6">post</a> by <a href="https://www.leadingsapiens.com/about/">Sheril Mathews</a>. Common thinking errors and how to spot them.
+      First, the ones I suspect I fall into the most:
+    </p>
+    <ul>
+      <li>Mind reading - "knowing" what others think, or assuming they know what you think. In my case, both.</li>
+      <li>Jumping to conclusions - x therefore y with little evidence. This seems especially hazardous in combination with the previous -- x therefore y, with inadequate evidence provided by mind reading!</li>
+      <li>Mental filtering - exclusive focus on negative (or positive), e.g. "that one thing I did wrong". Also works in combination with the others. Focus on the one negative thing that I think I mind read from someone else, and then jump to conclusions about it.</li>
+    </ul>
+    <p>Well, good to know oneself... Here are the others. Not saying I don't fall into these traps too... just that the ones above seem like my biggest hazards.</p>
+    <ul>
+      <li>All or nothing thinking - perfect or total failure, always, never</li>
+      <li>Fortune telling - usually negative, foregone conclusion, defeatism</li>
+      <li>Overgeneralization - from one or two data points, e.g. Don Music "I'll never get it, never!"</li>
+      <li>Should statements - unrealistic expectations</li>
+      <li>Disqualifying positive - success is a fluke/unreliable, e.g. "they were just being polite"</li>
+      <li>Magnification/Minimization - exaggerating or downplaying importance of something</li>
+      <li>Catastrophizing - assuming the worst, mountains out of molehills</li>
+      <li>Emotional reasoning - mistaking feelings for facts</li>
+      <li>Personalization/Self-blame - it's my fault</li>
+      <li>Labeling/Mislabeling - extreme overgeneralization</li>
+      <li>Always/Never thinking - seems like overgeneralization + mind reading</li>
+      <li>Entitlement - unrealistic expectations - see also Should</li>
+      <li>Outsourcing happiness - I'll be happy when...</li>
+      <li>Control fallacy - I control nothing (or everything)</li>
+      <li>Low frustration tolerance - what it says on the box</li>
+      <li>Fairness fallacy - life should always be fair and just - resentment, frustration</li>
+
+    </ul>
     <h2>Multiple new things at once</h2>
     <p>
       I'm used to using `npm` and Create React App with React/JavaScript to set up the UI. I decided I'd
